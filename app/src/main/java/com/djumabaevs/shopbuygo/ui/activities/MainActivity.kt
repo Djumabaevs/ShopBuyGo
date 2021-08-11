3d4fd270c5c41d385adff3eb9c5d1b0b3bb4dc5c
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-11 10:36:29 +0600

    handler to start splash screen

--- app/src/main/java/com/djumabaevs/shopbuygo/ui/activities/MainActivity.kt
+++ app/src/main/java/com/djumabaevs/shopbuygo/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.djumabaevs.shopbuygo
+package com.djumabaevs.shopbuygo.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.djumabaevs.shopbuygo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
